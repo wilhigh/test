d9b7c26bc42f650476fbfcf07f27b022864c6031
wilhigh <wilhigh77@gmail.com>
2022-02-16 14:35:49 +0900

    Add files via upload

---
+++ src/main/java/id/test/springboottesting/exception/UserRegistrationException.java
@@ -0,0 +1,18 @@
+package id.test.springboottesting.exception;
+
+/***
+ * Project Name     : spring-boot-testing
+ * Username         : Teten Nugraha
+ * Date Time        : 12/18/2019
+ * Telegram         : @tennugraha
+ */
+
+public class UserRegistrationException extends RuntimeException {
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+
+    public UserRegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
